5735e1b6aa03b5b7f26d4acf7d0abbc1e1e9cd75
Umair Khalid <umair@zaviago.com>
2023-08-28 15:01:26 +0500

    clinton

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -30,6 +30,17 @@ import banner3 from "../img/banner3.webp";
 import banner3Ico from "../img/banner3Ico.webp";
 import belowBanner from "../img/belowBanner.webp";
 import singleimage from "../img/singleimage.webp";
+import drillmachine from "../img/drillmachine.webp";
+import bottom1 from "../img/bottom1.webp";
+import bottom2 from "../img/bottom2.webp";
+import bottom3 from "../img/bottom3.webp";
+import bottom4 from "../img/bottom4.webp";
+import blog1img from "../img/blog1img.webp";
+import blog2img from "../img/blog2img.webp";
+import blog3img from "../img/blog3img.webp";
+import blog4img from "../img/blog4img.webp";
+import clintonTryton from "../img/clintonTryton.webp";
+import trytonCylender from "../img/trytonCylender.webp";
 
 const solutions = [
   {
@@ -76,24 +87,77 @@ const resources = [
   { name: "Guides", href: "#", icon: BookmarkSquareIcon },
   { name: "Webinars", href: "#", icon: ComputerDesktopIcon },
 ];
-const blogPosts = [
+// const blogPosts = [
+//     {
+//         date: "สิงหาคม 25, 2023",
+//         title: "2 เคล็ดไม่ลับ กำจัดตะไคร่น้ำฝังแน่น ให้หายวับในพริบตา",
+//         href: "#",
+//         desc: "เคล็ดไม่ลับ กำจัดตะไคร่น้ำฝังแน่น ให้หายวับในพริบตา ตะไคร่น้ำ คือหนึ่งในภัยร้ายหน้าฝน ที่นอกจากจะสร้าง",
+//         imageUrl: blog1img
+//     },
+//     {
+//         date: "สิงหาคม 26, 2022",
+//         title: "3 สิ่งที่ควรรู้ก่อนเลือกซื้อ เครื่องขัดกระดาษทราย",
+//         href: "#",
+//         desc: "เทคนิคเลือกซื้อ เครื่องขัดกระดาษทราย เครื่องขัดกระดาษทราย โดยทั่วไปที่จำหน่ายในท้องตลาดมีอยู่",
+//         imageUrl: blog2img
+//     },
+//     {
+//         date: "สิงหาคม 24, 2022",
+//         title: "5 เหตุผล ทำไมต้องตู้เชื่อม TRYTON อินเวอร์เตอร์",
+//         href: "#",
+//         desc: "ทำไมต้อง ตู้เชื่อมอินเวอร์เตอร์ TRYTON ตู้เชื่อมไฟฟ้าอินเวอร์เตอร์ เครื่องมือที่ช่างส่วนใหญ่นิยมใช้ ระบ",
+//         imageUrl: blog3img
+//     },
+//     {
+//         date: "สิงหาคม 30, 2022",
+//         title: "หมดปัญหา ซ่อมเครื่องดูดฝุ่นบ่อย ด้วย 8 เทคนิคยืด",
+//         href: "#",
+//         desc: "เทคนิคยืดอายุการใช้งานเครื่องดูดฝุ่น หมดปัญหา ซ่อมเครื่องดูดฝุ่นบ่อย ด้วย 8 เทคนิคยืดอายุการใช้งาน เป็น",
+//         imageUrl: blog4img
+//     },
+// ];
+
+const posts = [
   {
-    id: 1,
-    name: "Boost your conversion rate",
+    title: "Boost your conversion rate",
     href: "#",
-    preview:
-      "Eget ullamcorper ac ut vulputate fames nec mattis pellentesque elementum. Viverra tempor id mus.",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto accusantium praesentium eius, ut atque fuga culpa, similique sequi cum eos quis dolorum.",
+    date: "Mar 16, 2020",
     imageUrl:
-      "https://images.unsplash.com/photo-1558478551-1a378f63328e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2849&q=80",
+      "https://images.unsplash.com/photo-1496128858413-b36217c2ce36?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1679&q=80",
+    readingTime: "6 min",
   },
   {
-    id: 2,
-    name: "How to use search engine optimization to drive traffic to your site",
+    title: "How to use search engine optimization to drive sales",
     href: "#",
-    preview:
-      "Eget ullamcorper ac ut vulputate fames nec mattis pellentesque elementum. Viverra tempor id mus.",
+    category: { name: "Video", href: "#" },
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit facilis asperiores porro quaerat doloribus, eveniet dolore. Adipisci tempora aut inventore optio animi., tempore temporibus quo laudantium.",
+    date: "Mar 10, 2020",
     imageUrl:
-      "https://images.unsplash.com/photo-1486312338219-ce68d2c6f44d?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2300&q=80",
+      "https://images.unsplash.com/photo-1547586696-ea22b4d4235d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1679&q=80",
+  },
+  {
+    title: "Improve your customer experience",
+    href: "#",
+    category: { name: "Case Study", href: "#" },
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint harum rerum voluptatem quo recusandae magni placeat saepe molestiae, sed excepturi cumque corporis perferendis hic.",
+    date: "Feb 12, 2020",
+    imageUrl:
+      "https://images.unsplash.com/photo-1492724441997-5dc865305da7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1679&q=80",
+  },
+  {
+    title: "Improve your customer experience",
+    href: "#",
+    category: { name: "Case Study", href: "#" },
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint harum rerum voluptatem quo recusandae magni placeat saepe molestiae, sed excepturi cumque corporis perferendis hic.",
+    date: "Feb 12, 2020",
+    imageUrl:
+      "https://images.unsplash.com/photo-1492724441997-5dc865305da7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1679&q=80",
   },
 ];
 
@@ -114,7 +178,7 @@ export default function MainPage() {
         </button>
         <iframe
           className="w-full mt-10 mb-10"
-          height="515"
+          height="850"
           src="https://www.youtube.com/embed/574O2eLqui0?si=86wbj2Y12AYcTzAj"
           title="YouTube video player"
           frameborder="0"
@@ -122,16 +186,35 @@ export default function MainPage() {
           allowfullscreen
         ></iframe>
       </div>
-      <div className="px-[30px] first-letter hidden">
-        <div className="bg-[#F3F3F3] w-1/2 flex items-start rounded-[32px]">
-          <img
-            src={banner1Ico}
-            className="w-[337px] pt-[90px] h-[auto]"
-            alt=""
-          />
-          <img src={banner1} className="w-[470px] h-[auto]" alt="" />
+      <div className="px-[30px] first-letter grid grid-cols-2 gap-6">
+
+        <div className="bg-[#F3F3F3] flex flex-col items-start rounded-[32px] pt-[50px] pb-[30px]">
+		  <div className="ml-[40px]">
+            <img
+              src={banner1Ico}
+              className="w-[337px] pt-[90px] h-[auto]"
+              alt=""
+            />
+          </div>
+          <div className="flex justify-end -mt-[200px]">
+            <img src={banner1} className="w-[70%] h-[auto]" alt="" />
+          </div>
         </div>
-        <div className="bg-[#F3F3F3] w-1/2 flex"></div>
+
+        <div className="bg-[#384B94] items-center rounded-[32px] flex">
+          <div>
+            <img src={clintonTryton} className="w-[90%] ml-auto" alt="" />
+          </div>
+          <div>
+            <img src={trytonCylender} className="w-[411px]" alt="" />
+          </div>
+        </div>
+
+		<div className="flex flex-col items-center justify-center h-[580px]">
+			<h2 className="text-[#27387A] font-bold text-[46px] leading-[58px]">ปั๊มน้ำคลินตัน อึด แกร่ง แรงสุดขั้ว</h2>
+			<p className="text-[#27387A] text-[26px] leading-[38px] font-bold">เครื่องปั๊มน้ำ ต้องคลินตัน ด้วยสมรรถนะที่เหนือกว่าใคร ความแรงไม่มีตก ทำงานต่อเนื่องได้ไม่มีสะดุด ทำให้ ‘ปั๊มน้ำคลินตัน’ เป็นสินค้าขายดีอันดับหนึ่ง</p>
+		</div>
+
       </div>
 
       <div className="flex justify-center items-start w-[1270px] mx-auto">
@@ -147,7 +230,7 @@ export default function MainPage() {
         <img className="w-[700px] mt-44 -ml-56 " src={belowBanner} alt="" />
       </div>
 
-      <div className="w-[1270px] mx-auto mb-10">
+      <div className="sm:w-[1270px] mx-auto mb-10">
         <h2 className="text-[70px] leading-[94px] font-bold heading-gradient ">
           เครื่องมือช่าง CLINTON
         </h2>
@@ -163,8 +246,96 @@ export default function MainPage() {
         </p>
       </div>
 
-      <div className="w-[760px] mx-auto">
+      <div className="w-[full] bg-[#243576]">
+        <div className="flex sm:flex-row flex-col-reverse mx-[auto] sm:w-[1512px] bg-[#243576] pt-[80px] sm:pt-[140px] mt-[160px] px-4">
+          <div className="sm:w-[60%]">
+            <div className="-mb-[50px] -ml-[100px] sm:mt-0 mt-[50px]">
+              <img src={drillmachine} />
+            </div>
+          </div>
+          <div className="sm:w-[40%] sm:pt-[150px]">
+            <h3 className="white-gradient text-[40px] sm:text-[79px] font-bold leading-[56px] sm:leading-[100px]">
+              โปรโมชั่นประจำเดือน
+            </h3>
+            <p className="mt-5 sm:mr-[60px] text-[#b6c3f4] text-[18px] sm:text-2xl font-semibold leading-[38px]">
+              เพราะเรารู้ว่า คุณต้องการอะไร เราจึงตั้งใจคัดสรร ‘ปั๊มน้ำ
+              เครื่องมือช่างคุณภาพดี’ โดยคำนึงถึงความหลากหลาย
+              ของรูปแบบการใช้งานเพื่อนำไปสู่การปฏิบัติงาน ช่างที่สมบูรณ์แบบ
+              และนี่คือจุดเด่นสำคัญของเรา
+            </p>
+          </div>
+        </div>
+      </div>
+
+      <div className="sm:w-[760px] pt-[80px] mx-auto bg-white relative">
         <img src={singleimage} alt="" />
+      </div>
+
+      <div className="flex justify-between mt-[80px]">
+        <img src={bottom1} className="w-[210px]" alt="" />
+        <img src={bottom2} className="w-[240px]" alt="" />
+      </div>
+
+      <div className="px-6 md:-mt-[130px] pb-[40px] lg:px-8">
+        <div className="relative mx-auto max-w-7xl">
+          <div className="text-center">
+            <h3 className="text-[96px] leading-[94px] font-bold tracking-[-2.1px] heading-gradient text-center">
+              บทความ ข่าวสาร
+            </h3>
+          </div>
+          <div className="mx-auto mt-12 grid max-w-lg gap-7 lg:max-w-[1110px] lg:grid-cols-4">
+            {posts.map((post) => (
+              <div
+                key={post.title}
+                className="flex flex-col overflow-hidden border border-solid border-gray-300"
+              >
+                <div className="flex-shrink-0">
+                  <img
+                    className="h-48 w-full object-cover"
+                    src={post.imageUrl}
+                    alt=""
+                  />
+                </div>
+                <div className="flex flex-1 flex-col justify-between bg-white p-6">
+                  <div className="flex-1">
+                    <p className="text-[10px] leading-[28px] font-medium text-black  ">
+                      <a href={post.href} className="hover:underline">
+                        {post.date}
+                      </a>
+                    </p>
+                    <a href={post.href} className="block">
+                      <p className="text-[17px] leading-[26px] font-semibold text-[#bcbcbc] line-clamp-2">
+                        {post.title}
+                      </p>
+                      <p className="mt-1 text-[13px] leading-[23px] font-medium text-black line-clamp-3">
+                        {post.description}
+                      </p>
+                    </a>
+                  </div>
+                  <div className="mt-2 flex items-center">
+                    <div className="flex-shrink-0">
+                      <a href={post.href}>
+                        <span className="text-[#adadad] text-sm font-medium capitalize">
+                          Read more
+                        </span>
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
+          <div className="text-center">
+            <button className="bg-[#263676] text-sm rounded-lg text-center w-[150px] mt-10 text-white h-[40px]">
+              อ่านบทความเพิ่มเติม
+            </button>
+          </div>
+        </div>
+      </div>
+
+      <div className="flex justify-between mb-[180px]">
+        <img src={bottom3} className="w-[210px]" alt="" />
+        <img src={bottom4} className="w-[240px]" alt="" />
       </div>
     </>
   );
